24cac8b0320d501552a8e783518b8b05a8b52607
Austine Huang <austine.huang@gmail.com>
2025-11-15 17:29:19 +0800

    Initial Commit

---
+++ bookOfAnswers/bookOfAnswersApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookOfAnswersApp.swift
+//  bookOfAnswers
+//
+//  Created by Austine Huang on 2025/11/15.
+//
+
+import SwiftUI
+
+@main
+struct bookOfAnswersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
